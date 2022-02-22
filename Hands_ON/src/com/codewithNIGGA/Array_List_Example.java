dda927bae6d12242372c51080199bb14957f5a1a
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-02-23 00:09:34 +0530

    upto column with no specific size

---
+++ Hands_ON/src/com/codewithNIGGA/Array_List_Example.java
@@ -0,0 +1,20 @@
+package com.codewithNIGGA;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Array_List_Example {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // we use array list when we dont know how much size the user wants
+        // or how many nos the user will enter
+
+        //Syntax
+        ArrayList<Integer> list = new ArrayList<Integer>(10); // initial capacity means that it will take in minimum of 10 elements
+        // in the right side ArrayList<Integer>() the data type is not necessary
+        // we can also leave it blank as ArrayList<>();
+
+        
+
+    }
+}
--- Hands_ON/src/com/codewithNIGGA/Col_NO_Fixed.java
+++ Hands_ON/src/com/codewithNIGGA/Col_NO_Fixed.java
@@ -1,7 +1,28 @@
 package com.codewithNIGGA;
+
+import java.util.Scanner;
+
 //Q: column having no fixed size
 public class Col_NO_Fixed {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        int[][] arr = {
+                {1,2,3},
+                {4,5},
+                {6,7,8,9}
+        };
+        for (int row = 0; row < arr.length ; row++) {
+            for (int col = 0; col < arr[row].length ; col++)
+            // arr[row].length is used
+                // whenever there is no fixed size of the column
+
+            {
+                System.out.print(arr[row][col] + " ");
+                // println is for new line
+                // print is for single line outputting
+            }
+            System.out.println();
+
+        }
     }
 }
