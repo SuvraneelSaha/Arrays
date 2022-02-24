f256df4230deb4d7da3f656e1cbb5c128c0263b5
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-02-24 20:18:46 +0530

    updated multi dimension array list

--- Hands_ON/src/com/codewithNIGGA/Multidimension_Array_List.java
+++ Hands_ON/src/com/codewithNIGGA/Multidimension_Array_List.java
@@ -1,9 +1,11 @@
 package com.codewithNIGGA;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Multidimension_Array_List {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
         // we are just making a 2d arraylist
         // just similar to an int[][] integer 2d array
@@ -22,6 +24,31 @@ public class Multidimension_Array_List {
         }
         // we are initializing  3 array lists
         // inside of a single array list
-        
+
+        for (int i = 0; i <3 ; i++) {
+            for (int j = 0; j < list.size() ; j++)
+            // list.size() for knowing the size of the lenght of the row just like in the case of an int[][] array
+
+
+            {
+                list.get(i).add(in.nextInt());
+
+
+            }
+
+        }
+
+        System.out.println(list);
+
+
+
     }
 }
+//IMP
+// we can actually use the 2d array in case of storing many array inside of a single array
+// just like the int[][] array where each index element is a array itself
+//  so
+// THE ARRAYLIST IS UST LIKE AN ARRAY
+// &&
+// THE MULTI DIMENSION ARRAYLIST IS LIKE 2D ARRAY
+
