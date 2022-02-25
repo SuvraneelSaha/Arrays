56b09b0132db07819660f7891b951a79c863b33e
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-02-25 12:49:53 +0530

    practice easy creation

---
+++ Practice_EASY/src/com/codewithSuvro/Main.java
@@ -0,0 +1,10 @@
+package com.codewithSuvro;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello practice arrays !!");
+
+    }
+}
